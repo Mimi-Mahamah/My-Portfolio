5447e2e9ac53195be8ff8e72cc7f1b31d4a0f4ee
mimi-mahamah <mimi-mahamah@github.com>
2022-01-13 11:36:46 -0800

    final fix to media queries and improved page layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ export default function Projects() {
             <>
             <img src="./sodelicious-screenshot.png" alt="so delicious screenshot" id="sodelicious" />
             </>
-            <p className="project-1-about">I worked on this website with a team of 5.</p>
+            <p className="project-1-about">This web app was created with a team of 5, including 3 Frontend Developers, a backend developer and a Designer. The project was created to highlight recipes across the diaspora that are not regularly seen in popular media.</p>
             <a href="https://github.com/Mimi-Mahamah/so-delicious" target="blank" rel="noreferrer">
                 <img src="./github.svg" alt="github icon" className="github-icon" />
             </a>
@@ -16,7 +16,7 @@ export default function Projects() {
             <a href="https://github.com/Mimi-Mahamah/My-Portfolio" target = "blank" rel="noreferrer">
                 <img src="./github.svg" alt="github icon" className="github-icon2" />
             </a>
-            <p className="project-2-about">This web app was created using react.</p>
+            <p className="project-2-about">This web app was created using React, HTML and CSS.</p>
             
             <h1 className="coming-soon">More Projects Coming Soon!</h1>
         </>
