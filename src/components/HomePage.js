5876ec988498b36343a8341884dd31218ea7d2f7
mimi-mahamah <mimi-mahamah@github.com>
2023-01-09 20:26:28 -0800

    Updated headings to be more accessible and made changes to font-sizes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,14 +6,15 @@ export default function HomePage() {
         <>
             <div className="sidebar"></div>
             <header>
-                <h1 id="name">Mimi Mahamah</h1>
-                <h1 id="projects">Projects</h1>
-                <h1 id="contact">Contact</h1>
+                <h1 className="name">Mimi Mahamah</h1>
+                <a href="" className="projects">Projects</a>
+                <a href="" className="contact">Contact</a>
             </header>
             <div className="about">
             <h2>Front End Developer</h2>
             <p className="blurb">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque corporis enim eum impedit animi doloribus. <br/>
             Mollitia dignissimos hic et fuga ut. Dolorum optio porro recusandae tempora amet itaque dolorem dignissimos?</p>
+            <a href="">More About Me</a>
             </div>
         </>
     )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,22 +12,22 @@ export default function Skills() {
 
         <div className="skill-icons">
         <img src="./html-5.svg" className="skill-svg" alt="html icon" />
-        <h4>HTML</h4>
+        <p className="skill-titles"> HTML</p>
         </div>
 
         <div className="skill-icons">
         <img src="./javascript.svg" className="skill-svg" alt="javascript icon"/>
-        <h4> JavaScript</h4>
+        <p className="skill-titles"> JavaScript</p>
         </div>
 
         <div className="skill-icons">
         <img src="./css3.svg" className="skill-svg" alt="css icon" />
-        <h4>CSS</h4>
+        <p className="skill-titles">CSS</p>
         </div>
         
         <div className="skill-icons">
         <img src="./react-native.svg" className="skill-svg" alt="react icon" />
-        <h4>React</h4>
+        <p className="skill-titles">React</p>
         </div>
         </section>
 
@@ -35,17 +35,17 @@ export default function Skills() {
       
         <div className="skill-icons">
         <img src="./git.svg" className="skill-svg" alt="git icon" />
-        <h4>Git</h4>
+        <p className="skill-titles">Git</p>
         </div>
 
         <div className="skill-icons">
           <img src="./github2.svg" className="skill-svg" alt="github icon" />
-          <h4>Github</h4>
+          <p className="skill-titles">Github</p>
         </div>
 
         <div className="skill-icons">
           <img src="./vscode.svg" className="skill-svg" alt="vscode icon" />
-          <h4>VS Code</h4>
+          <p className="skill-titles">VS Code</p>
         </div>
         </section>
       </>
